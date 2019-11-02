9b3cc4ce5f54d8652358e65066c3a66b16325a25
Danilo Augusto <34426848+daniloamsilva@users.noreply.github.com>
2019-11-02 10:01:58 -0300

    Add files via upload

---
+++ Aulas PHP7/Aula 296 - Embutindo blocos PHP em páginas HTML.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Aula 296</title>
+</head>
+<body>
+    
+    <?php
+        echo 'Utilizando a tag padrão';
+    ?>
+    <br>
+    <?= 'Utilizando a tag de impressão' ?>
+
+</body>
+</html>
