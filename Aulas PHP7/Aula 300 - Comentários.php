8fb515dd3734a321962e27dc4380b1d8ce843429
Danilo Augusto <34426848+daniloamsilva@users.noreply.github.com>
2019-11-04 19:53:21 -0300

    Add files via upload

---
+++ Aulas PHP7/Aula 300 - Comentários.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <!-- Este comentário aparece do lado cliente -->
+
+    <?php
+        // Comentário de uma linha
+        echo 'Comando eco <br>';
+
+        # Outro comentário de uma linha
+        print 'Comando print <hr>';
+
+        /*
+            Este é um comentário de múltiplas linhas.
+            Este tipo de comentário permite que
+            várias linhas sejam comentadas ao mesmo tempo.
+        */ 
+    ?>
+
+</body>
+</html>
