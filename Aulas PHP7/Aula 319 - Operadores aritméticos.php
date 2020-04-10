64885a7e1d1ebc65e0798639592e6998a26f4cc3
daniloamsilva <danilo.augmonteiro@gmail.com>
2020-04-10 13:27:16 -0300

    Create Aula 319 - Operadores aritméticos.php

---
+++ Aulas PHP7/Aula 319 - Operadores aritméticos.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>Operadores aritméticos</h1>
+
+    <?php
+        
+        $num1 = 13;
+        $num2 = 4;
+
+        echo 'A soma entre 13 e 4 é ' . ($num1 + $num2) . '. <br>';
+        echo 'A subtração entre 13 e 4 é ' . ($num1 - $num2) . '. <br>';
+        echo 'A multiplicação entre 13 e 4 é ' . ($num1 * $num2) . '. <br>';
+        echo 'A divisão entre 13 e 4 é ' . ($num1 / $num2) . '. <br>';
+        echo 'O resto da divisão entre 13 e 4 é ' . ($num1 % $num2) . '. <br>';
+
+    ?>
+
+</body>
+</html>
