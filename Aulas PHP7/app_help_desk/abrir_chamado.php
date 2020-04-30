4a56fc55b1eb73fefdeb0a5c089d8245ec6d48dd
daniloamsilva <danilo.augmonteiro@gmail.com>
2020-04-30 09:37:50 -0300

    Aula PHP

--- Aulas PHP7/app_help_desk/abrir_chamado.php
+++ Aulas PHP7/app_help_desk/abrir_chamado.php
@@ -23,6 +23,11 @@
         <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
         App Help Desk
       </a>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="logoff.php">SAIR</a>
+        </li>
+      </ul>
     </nav>
 
     <div class="container">    
@@ -61,7 +66,7 @@
 
                     <div class="row mt-5">
                       <div class="col-6">
-                        <button class="btn btn-lg btn-warning btn-block" type="submit">Voltar</button>
+                        <a class="btn btn-lg btn-warning btn-block" href="home.php">Voltar</a>
                       </div>
 
                       <div class="col-6">
