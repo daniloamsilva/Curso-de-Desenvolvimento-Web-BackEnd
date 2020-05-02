ae4bf7fa28cf13be1df935012d6af4b91e2d327c
daniloamsilva <danilo.augmonteiro@gmail.com>
2020-05-02 11:11:20 -0300

    Aula PHP

--- Aulas PHP7/app_help_desk/consultar_chamado.php
+++ Aulas PHP7/app_help_desk/consultar_chamado.php
@@ -1,5 +1,20 @@
 <? require_once 'validador_acesso.php' ?>
 
+<?php
+
+  $chamados = [];
+
+  $arquivo = fopen('arquivo.hd', 'r');
+
+  while(!feof($arquivo)){
+    $registro = fgets($arquivo);
+    $chamados[] = $registro;
+  }
+
+  fclose($arquivo);
+
+?>
+
 <html>
   <head>
     <meta charset="utf-8" />
@@ -41,23 +56,25 @@
             
             <div class="card-body">
               
-              <div class="card mb-3 bg-light">
-                <div class="card-body">
-                  <h5 class="card-title">Título do chamado...</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                  <p class="card-text">Descrição do chamado...</p>
+              <? foreach($chamados as $chamado) { 
+                
+                  $chamados_dados = explode('#', $chamado);
 
+                  if(count($chamados_dados) < 3){
+                    continue;
+                  }
+              ?>
+
+                <div class="card mb-3 bg-light">
+                  <div class="card-body">
+                    <h5 class="card-title"><?= $chamados_dados[0] ?></h5>
+                    <h6 class="card-subtitle mb-2 text-muted"><?= $chamados_dados[1] ?></h6>
+                    <p class="card-text"><?= $chamados_dados[2] ?></p>
+
+                  </div>
                 </div>
-              </div>
 
-              <div class="card mb-3 bg-light">
-                <div class="card-body">
-                  <h5 class="card-title">Título do chamado...</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                  <p class="card-text">Descrição do chamado...</p>
-
-                </div>
-              </div>
+              <? } ?>
 
               <div class="row mt-5">
                 <div class="col-6">
