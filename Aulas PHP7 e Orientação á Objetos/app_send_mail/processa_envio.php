58400aedbce768da22440746903bf540dfcd1218
daniloamsilva <danilo.augmonteiro@gmail.com>
2020-05-23 11:41:19 -0300

    Update processa_envio.php

--- Aulas PHP7 e Orientação á Objetos/app_send_mail/processa_envio.php
+++ Aulas PHP7 e Orientação á Objetos/app_send_mail/processa_envio.php
@@ -1,2 +1,38 @@
 <?php
-    print_r($_POST);
+    
+    class Mensagem {
+        
+        private $para = null;
+        private $assunto = null;
+        private $mensagem = null;
+
+        public function __get($atributo){
+            return $this->$atributo;
+        }
+
+        public function __set($atributo, $valor){
+            $this->$atributo = $valor;
+        }
+
+        public function mensagemValida(){
+            if (empty($this->para) || empty($this->assunto) || empty($this->mensagem)) {
+                return false;
+            }
+            return true;
+        }
+
+    }
+
+    $mensagem = new Mensagem();
+
+    $mensagem->para = $_POST['para'];
+    $mensagem->assunto = $_POST['assunto'];
+    $mensagem->mensagem = $_POST['mensagem'];
+
+    if ($mensagem->mensagemValida()) {
+        echo 'Mensagem é válida';
+    }else {
+        echo 'Mensagem não é válida';
+    }
+
+?>
