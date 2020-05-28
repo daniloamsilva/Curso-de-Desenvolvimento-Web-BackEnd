f7bcb561ac39632dcd3e49096480cdf76328571e
daniloamsilva <danilo.augmonteiro@gmail.com>
2020-05-28 18:53:46 -0300

    Aula PHP

---
+++ Aulas PHP7 e Orientação á Objetos/app_send_mail/app_send_mail/processa_envio.php
@@ -0,0 +1,131 @@
+<?php
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
+    
+    class Mensagem {
+        
+        private $para = null;
+        private $assunto = null;
+        private $mensagem = null;
+        public $status = ['codigo_status' => null, 'descricao_status' => null];
+
+        public function __get($atributo){
+            return $this->$atributo;
+        }
+
+        public function __set($atributo, $valor){
+            $this->$atributo = $valor;
+        }
+
+        public function mensagemValida(){
+            if (empty($this->para) || empty($this->assunto) || empty($this->mensagem)) {
+                return false;
+            }
+            return true;
+        }
+
+    }
+
+    $mensagem = new Mensagem();
+
+    $mensagem->para = $_POST['para'];
+    $mensagem->assunto = $_POST['assunto'];
+    $mensagem->mensagem = $_POST['mensagem'];
+
+    if (!$mensagem->mensagemValida()) {
+        echo 'Mensagem não é válida';
+        header('location: index.php');
+    }
+
+    // Instantiation and passing `true` enables exceptions
+    $mail = new PHPMailer(true);
+
+    try {
+        //Server settings
+        $mail->SMTPDebug = false;                      // Enable verbose debug output
+        $mail->isSMTP();                                            // Send using SMTP
+        $mail->Host       = 'smtp.google.com';                    // Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+        $mail->Username   = 'appsendmaildams@gmail.com';                     // SMTP username
+        $mail->Password   = 'appsendmail1!';                               // SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+        $mail->Port       = 587;                                  // TCP port to connect to
+
+        //Recipients
+        $mail->setFrom('appsendmaildams@gmail.com', 'Web Completo Remetente');
+        $mail->addAddress($mensagem->para);     // Add a recipient
+        //$mail->addAddress('ellen@example.com');               // Name is optional
+        //$mail->addReplyTo('info@example.com', 'Information');
+        //$mail->addCC('cc@example.com');
+        //$mail->addBCC('bcc@example.com');
+
+        // Attachments
+        //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+        //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+        
+        
+        // Content
+        $mail->isHTML(true);                                  // Set email format to HTML
+        $mail->Subject = $mensagem->assunto;
+        $mail->Body    = $mensagem->mensagem;
+        $mail->AltBody = 'É necessário utilizar um client que suporte HTML para ter acesso ao conteúdo dessa msg.';
+
+        $mail->send();
+
+        $mensagem->status['codigo_status'] = 1;
+        $mensagem->status['descricao_status'] = 'E-mail enviado com sucesso';
+    } catch (Exception $e) {
+        $mensagem->status['codigo_status'] = 2;
+        $mensagem->status['descricao_status'] = "Não foi possível enviar o e-mail. Detalhes do erro:{$mail->ErrorInfo}";
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Send Mail</title>
+</head>
+<body>
+    <div class="container">
+        <div class="py-3 text-center">
+            <img class="d-block mx-auto mb-2" src="logo.png" alt="" width="72" height="72">
+            <h2>Send Mail</h2>
+            <p class="lead">Seu app de envio de e-mails particular!</p>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12">
+
+                <? if ($mensagem->status['codigo_status'] == 1) { ?>
+                
+                    <div class="container">
+                        <h1 class="display-4 text-success">Sucesso</h1>
+                        <p><?= $mensagem->status['descricao_status'] ?></p>
+                        <a href="index.php" class="btn btn-success btn-lg mt-5 text-white">Voltar</a>
+                    </div>
+                
+                <? } ?>
+
+                <? if ($mensagem->status['codigo_status'] == 2) { ?>
+                
+                    <div class="container">
+                        <h1 class="display-4 text-danger">Ops!</h1>
+                        <p><?= $mensagem->status['descricao_status'] ?></p>
+                        <a href="index.php" class="btn btn-success btn-lg mt-5 text-white">Voltar</a>
+                    </div>
+                
+                <? } ?>
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ Aulas PHP7 e Orientação á Objetos/app_send_mail/www/app_send_mail/processa_envio.php
@@ -0,0 +1,7 @@
+<?php
+
+    require '../../app_send_mail/bibliotecas/PHPMailer/Exception.php';
+    require '../../app_send_mail/bibliotecas/PHPMailer/PHPMailer.php';
+    require '../../app_send_mail/bibliotecas/PHPMailer/SMTP.php';
+
+    require '../../app_send_mail/processa_envio.php';
