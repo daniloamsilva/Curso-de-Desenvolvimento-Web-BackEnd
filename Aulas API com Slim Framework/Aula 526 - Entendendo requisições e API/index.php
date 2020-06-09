573b7cadf2b8c072c797561849fa55c1746abbb7
daniloamsilva <danilo.augmonteiro@gmail.com>
2020-06-09 18:29:46 -0300

    Aula Slim Framework

---
+++ Aulas API com Slim Framework/Aula 526 - Entendendo requisições e API/index.php
@@ -0,0 +1,15 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+require 'vendor/autoload.php';
+
+$app = new \Slim\App;
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+    return $response;
+});
+
+$app->run();
