95e45d5d1be449739d873adf0e796a074f2d6d96
daniloamsilva <danilo.augmonteiro@gmail.com>
2020-09-08 21:37:52 -0300

    Update todas_tarefas.php

--- Aulas PHP com PDO e MySql/app_lista_tarefas/public/app_lista_tarefas_public/todas_tarefas.php
+++ Aulas PHP com PDO e MySql/app_lista_tarefas/public/app_lista_tarefas_public/todas_tarefas.php
@@ -18,6 +18,61 @@
 		<link rel="stylesheet" href="css/estilo.css">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+		<script>
+			function editar(id, txt_tarefa){
+				// criar um form de edição
+				let form = document.createElement('form')
+				form.action = '#'
+				form.method = 'post'
+				form.className = 'row'
+
+				// criar um input para entrada do texto
+				let inputTarefa = document.createElement('input')
+				inputTarefa.type = 'text'
+				inputTarefa.name = 'tarefa'
+				inputTarefa.className = 'col-9 form-control'
+				inputTarefa.value = txt_tarefa
+
+				// criar um input hidden para guardar o id da tarefa
+				let inputId = document.createElement('input')
+				inputId.type = 'hidden'
+				inputId.name = 'id'
+				inputId.value = id
+
+				// criar um button para envio do form
+				let button = document.createElement('button')
+				button.type = 'submit'
+				button.className = 'col-3 btn btn-info'
+				button.innerHTML = 'Atualizar'
+
+				// incluir inputTarefa no form
+				form.appendChild(inputTarefa)
+
+				// incluir inputId no form
+				form.appendChild(inputId)
+
+				// incluir button no form
+				form.appendChild(button)
+
+				// teste
+				//console.log(form)
+
+				//alert(id)
+
+				// seleciona a div tarefa
+				let tarefa = document.getElementById('tarefa_'+id)
+
+				// limpar o texto da tarefa para inclusão do form
+				tarefa.innerHTML = ''
+
+				// incluir form na página
+				tarefa.insertBefore(form, tarefa[0])
+
+				//alert(txt_tarefa)
+
+			}
+		</script>
 	</head>
 
 	<body>
@@ -50,10 +105,12 @@
 								<? foreach($tarefas as $indice => $tarefa) { ?>
 
 								<div class="row mb-3 d-flex align-items-center tarefa">
-									<div class="col-sm-9"><?= $tarefa->tarefa ?>(<?= $tarefa->status ?>)</div>
+									<div class="col-sm-9" id="tarefa_<?= $tarefa->id ?>">
+										<?= $tarefa->tarefa ?>(<?= $tarefa->status ?>)
+									</div>
 									<div class="col-sm-3 mt-2 d-flex justify-content-between">
 										<i class="fas fa-trash-alt fa-lg text-danger"></i>
-										<i class="fas fa-edit fa-lg text-info"></i>
+										<i class="fas fa-edit fa-lg text-info" onclick="editar(<?= $tarefa->id ?>, '<?= $tarefa->tarefa ?>')"></i>
 										<i class="fas fa-check-square fa-lg text-success"></i>
 									</div>
 								</div>
