0b55dbd05448f5fd6bd7659be568b1596b555eab
daniloamsilva <danilo.augmonteiro@gmail.com>
2020-10-29 18:32:50 -0300

    Projeto Dashboard

--- Aulas JQuery/Projeto Dashboard/script.js
+++ Aulas JQuery/Projeto Dashboard/script.js
@@ -1,3 +1,23 @@
 $(document).ready(() => {
-	
+    
+    // Método load()
+    //$('#documentacao').on('click', () => $('#pagina').load('documentacao.html'));
+    //$('#suporte').on('click', () => $('#pagina').load('suporte.html'));
+
+    // Método get
+    /*$('#documentacao').on('click', () => {
+        $.get('documentacao.html', data => $('#pagina').html(data));
+    });
+    $('#suporte').on('click', () => {
+        $.get('suporte.html', data => $('#pagina').html(data));
+    });*/
+
+    // Método post
+    $('#documentacao').on('click', () => {
+        $.post('documentacao.html', data => $('#pagina').html(data));
+    });
+    $('#suporte').on('click', () => {
+        $.post('suporte.html', data => $('#pagina').html(data));
+    });
+
 })
