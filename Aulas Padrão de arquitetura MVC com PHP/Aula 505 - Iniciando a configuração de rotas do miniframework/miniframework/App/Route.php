3e2a3072f193a1ae76d43d8e1157dbd5e1bc5c38
daniloamsilva <danilo.augmonteiro@gmail.com>
2020-11-06 19:38:35 -0300

    Aula 505

---
+++ Aulas Padrão de arquitetura MVC com PHP/Aula 505 - Iniciando a configuração de rotas do miniframework/miniframework/App/Route.php
@@ -0,0 +1,27 @@
+<?php
+    namespace App;
+
+    class Route {
+
+        public function initRoutes(){
+
+            $routes['home'] = array(
+                'route' => '/',
+                'controller' => 'indexController',
+                'action' => 'index'
+            );
+
+            $routes['sobre_nos'] = array(
+                'route' => '/sobre_nos',
+                'controller' => 'indexController',
+                'action' => 'sobreNos'
+            );
+
+        }
+
+        public function getUrl(){
+            return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+        }
+
+    }
+?>
