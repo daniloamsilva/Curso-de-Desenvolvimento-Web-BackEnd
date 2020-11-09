5930309f3df56e71a426e953336aab7a6266e690
daniloamsilva <danilo.augmonteiro@gmail.com>
2020-11-09 18:39:53 -0300

    Aula 506

---
+++ Aulas Padrão de arquitetura MVC com PHP/Aula 506 - Controllers/miniframework/App/Controllers/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+    namespace App\Controllers;
+
+    class IndexController {
+
+        public function index(){
+            echo 'Chegamos ao index';
+        }
+
+        public function sobreNos(){
+            echo 'Chegamos ao sobre nós';
+        }
+
+    }
+?>
