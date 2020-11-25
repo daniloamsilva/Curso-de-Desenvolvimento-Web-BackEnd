c33a963ca0e7bf0bad7fe9cc876adb19adffe0b0
daniloamsilva <danilo.augmonteiro@gmail.com>
2020-11-24 22:44:22 -0300

    Twitter Clone

--- Aulas PHP 7 MySQL - Projeto Twitter Clone/Projeto Twitter Clone/App/Controllers/IndexController.php
+++ Aulas PHP 7 MySQL - Projeto Twitter Clone/Projeto Twitter Clone/App/Controllers/IndexController.php
@@ -9,6 +9,7 @@ use MF\Model\Container;
 class IndexController extends Action {
 
 	public function index() {
+		$this->view->login = isset($_GET['login']) ? $_GET['login'] : '';
 		$this->render('index');
 	}
 
--- Aulas PHP 7 MySQL - Projeto Twitter Clone/Projeto Twitter Clone/App/Models/Usuario.php
+++ Aulas PHP 7 MySQL - Projeto Twitter Clone/Projeto Twitter Clone/App/Models/Usuario.php
@@ -51,6 +51,24 @@
             return $stmt->fetchAll(\PDO::FETCH_ASSOC);
         }
 
+        //autenticação de usuario
+        public function autenticar(){
+            $query = "SELECT id, nome, email FROM usuarios WHERE email = :email AND senha = :senha";
+            $stmt = $this->db->prepare($query);
+            $stmt->bindValue(':email', $this->__get('email'));
+            $stmt->bindValue(':senha', $this->__get('senha'));
+            $stmt->execute();
+
+            $usuario = $stmt->fetch(\PDO::FETCH_ASSOC);
+
+            if($usuario['id'] != '' && $usuario['nome'] != '') {
+                $this->__set('id', $usuario['id']);
+                $this->__set('nome', $usuario['nome']);
+            }
+
+            return $this;
+        }
+
     }
 
 ?>
